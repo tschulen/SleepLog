0b1cdb1b2e33ea15faf32aac72880ebbab45b34f
John Kang <jkang8@be-linux19.lt.ucsc.edu>
2015-02-05 16:43:47 -0800

    added categories to model

--- models/journal.py
+++ models/journal.py
@@ -5,6 +5,7 @@ db.define_table('entry',
         Field('title'),
         Field('body', 'text'), 
         Field('is_lucid', 'boolean'),
+        Field('category'),
         Field('user_id', db.auth_user),
         )
 
@@ -14,3 +15,5 @@ db.entry.date_posted.writable = False
 db.entry.is_lucid.default = False
 db.entry.user_id.default = auth.user_id
 db.entry.user_id.writable = db.entry.user_id.readable = False
+
+db.entry.category.requires = IS_IN_SET(['Normal', 'Nightmare', 'Lucid'])
